a97a85ada8f3adb41cf093d9730c65815de96d9e
FelipeCarvalhoLuz <felipe_carvalho-luz@estudante.sesisenai.org.br>
2025-11-18 12:17:30 -0300

    s2

--- S2/sketch_oct6a/sketch_oct6a.ino
+++ S2/sketch_oct6a/sketch_oct6a.ino
@@ -1,45 +1,111 @@
 #include <WiFi.h>
+#include <WiFiClientSecure.h>
 #include <PubSubClient.h>
 
-WiFiClient client;
-PubSubClient mqtt(client);
+const char* SSID = "FIESC_IOT_EDU";
+const char* PASS = "8120gv08";
 
-const String SSID = "FIESC_IOT_EDU";
-const String PASS = "8120gv08";
+const char* BROKER = "7aecec580ecf4e5cbac2d52b35eb85b9.s1.eu.hivemq.cloud";
+const int PORT = 8883;
 
-const int PORT               = 1883;
-const String URL               = "test.mosquitto.org";
-const String TOPIC            = "DSM2";
-const String broker_user      = "";
-const String broker_pass      = "";
+const char* BROKER_USER = "felipe";
+const char* BROKER_PASS = "FelipeDSM2";
+
+#define TRIG1 12
+#define ECHO1 25
+#define TRIG2 18
+#define ECHO2 21
+#define PINO_RGB 14
+#define PINO_LED 4
+
+const char* TOPICO_PUBLISH_1 = "Projeto S2 Distancia1";
+const char* TOPICO_PUBLISH_2 = "Projeto S2 Distancia2";
+const char* TOPICO_SUBSCRIBE = "S1 iluminacao";
+const char* TOPICO_ENVIO_S3   = "Projeto S3 Controle";
+
+WiFiClientSecure espClient;
+PubSubClient mqtt(espClient);
+
+long medirDistancia(int trigPin, int echoPin) {
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(5);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  long duracao = pulseIn(echoPin, HIGH, 30000);
+  long distancia = duracao * 0.034 / 2;
+  return distancia;
+}
+
+void callback(char* topic, byte* payload, unsigned int length) {
+  String mensagem;
+  for (int i = 0; i < length; i++) {
+    mensagem += (char)payload[i];
+  }
+  if (mensagem == "acender") {
+    digitalWrite(PINO_LED, HIGH);
+  } else if (mensagem == "apagar") {
+    digitalWrite(PINO_LED, LOW);
+  }
+  Serial.println(mensagem);
+}
+
+void conectaWiFi() {
+  WiFi.begin(SSID, PASS);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+  }
+  Serial.println("Conectado ao WiFi!");
+}
+
+void conectaMQTT() {
+  mqtt.setServer(BROKER, PORT);
+  mqtt.setCallback(callback);
+  espClient.setInsecure();
+  while (!mqtt.connected()) {
+    String clientId = "S2_" + String(random(0xffff), HEX);
+    if (mqtt.connect(clientId.c_str(), BROKER_USER, BROKER_PASS)) {
+      mqtt.subscribe(TOPICO_SUBSCRIBE);
+    } else {
+      delay(2000);
+    }
+  }
+  Serial.println("Conectado ao broker!");
+}
 
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(115200);
-  Serial.println("Conectando ao Wifi");
-  WiFi.begin(SSID,PASS);
-  while(WiFi.status() != WL_CONNECTED){
-    Serial.print(".");
-    delay(200);
-  }
-  Serial.println("\nConectado!");
-    Serial.println("Conectando ao broker...");
-    mqtt.setServer(URL.c_str(),PORT);
-   while(!mqtt.connected()){
-    String ID = "S1-";
-    ID += String(random(0xffff),HEX);
-    mqtt.connect(ID.c_str(),broker_user.c_str(),broker_pass.c_str());
-    delay(200);
-    Serial.print(".");
-}
-Serial.println("\n Conectado ao broker com sucesso!");
+  pinMode(TRIG1, OUTPUT);
+  pinMode(ECHO1, INPUT);
+  pinMode(TRIG2, OUTPUT);
+  pinMode(ECHO2, INPUT);
+  pinMode(PINO_LED, OUTPUT);
+  digitalWrite(PINO_LED, LOW);
+  conectaWiFi();
+  conectaMQTT();
 }
 
 void loop() {
-  String mensagem = "Joao: ";
-  mensagem += "oi";
+  if (!mqtt.connected()) {
+    conectaMQTT();
+  }
+  long dist1 = medirDistancia(TRIG1, ECHO1);
+  long dist2 = medirDistancia(TRIG2, ECHO2);
+  String msg1 = String(dist1);
+  String msg2 = String(dist2);
 
-  mqtt.publish(TOPIC.c_str(),mensagem.c_str());
+  Serial.println(dist1);
+  Serial.println(dist2);
+
+  if (dist1 < 10) {
+    mqtt.publish(TOPICO_PUBLISH_1, "objeto_proximo");
+    mqtt.publish(TOPICO_ENVIO_S3, "objeto_proximo");
+  }
+  if(dist2 > 10) {
+    mqtt.publish(TOPICO_PUBLISH_2, "objeto_longe");
+    mqtt.publish(TOPICO_ENVIO_S3, "objeto_longe");
+  }
+  
+  delay(1000);
   mqtt.loop();
-  delay(2000);
 }
